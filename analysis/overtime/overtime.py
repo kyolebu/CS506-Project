d203763ac6b6feadbb12ae09bf3afb04fc5a8b80
Eric Nohara-LeClair <ernohara@bu.edu>
2025-03-18 00:35:33 -0400

    added initial preprocessing of average yearly OT hours per employee

--- analysis/overtime/overtime.py
+++ analysis/overtime/overtime.py
@@ -1,5 +1,20 @@
 import pandas as pd
 
-df = pd.read_csv("../../data/overtime/2022-overtime.csv")
+year = 2012
+for _ in range(10):
+    year += 1
 
-print(df.head())
+    df = pd.read_csv(f"../../data/overtime/{year}-overtime.csv")
+
+    formatted_hours_worked = df["Hours Worked"].apply(lambda x: x / 100 if x >= 24 else x)
+    formatted_hours_paid = df["Hours Paid"].apply(lambda x: x / 100 if x >= 24 else x)
+    
+    df["Overtime Hours"] = formatted_hours_paid - formatted_hours_worked
+
+    # total_ot = sum(df["Overtime Hours"])
+    # avg_ot = total_ot / df.shape[0]
+
+    total_ot_per_employee = df.groupby("Emp. ID")["Overtime Hours"].sum()
+    avg_ot = total_ot_per_employee.mean()
+
+    print(f"Average Overtime Hours Per Employee in Year {year}: {avg_ot}")
