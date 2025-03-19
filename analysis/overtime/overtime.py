684388e88f798e94f9b405f9e6442b51e5787830
Eric Nohara-LeClair <ernohara@bu.edu>
2025-03-18 22:38:56 -0400

    added initial plotting for simple analysis of overtime data over time

--- analysis/overtime/overtime.py
+++ analysis/overtime/overtime.py
@@ -1,20 +1,42 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
-year = 2012
-for _ in range(10):
-    year += 1
+years = list(range(2012, 2023))
+totals = []
+avgs = []
+# rank_avgs = []
 
-    df = pd.read_csv(f"../../data/overtime/{year}-overtime.csv")
+for year in years:
+    df = pd.read_csv(f"../../data/overtime/{year}.csv")
 
-    formatted_hours_worked = df["Hours Worked"].apply(lambda x: x / 100 if x >= 24 else x)
-    formatted_hours_paid = df["Hours Paid"].apply(lambda x: x / 100 if x >= 24 else x)
-    
-    df["Overtime Hours"] = formatted_hours_paid - formatted_hours_worked
+    total_ot_per_employee = df.groupby("ID")["OTHOURS"].sum()
+    avg_employee_ot = total_ot_per_employee.mean()
+    avgs.append(avg_employee_ot)
 
-    # total_ot = sum(df["Overtime Hours"])
-    # avg_ot = total_ot / df.shape[0]
+    # total_ot_per_rank = df.groupby("RANK")["OTHOURS"].sum()
+    # avg_rank_ot = 
 
-    total_ot_per_employee = df.groupby("Emp. ID")["Overtime Hours"].sum()
-    avg_ot = total_ot_per_employee.mean()
+    total_ot = df["OTHOURS"].sum()
+    totals.append(total_ot)
 
     print(f"Average Overtime Hours Per Employee in Year {year}: {avg_ot}")
+
+# Average overtime plot
+plt.figure(figsize=(10, 5))
+plt.plot(years, avgs, marker='o', linestyle='-', color='b')
+plt.xlabel("Year")
+plt.ylabel("Average Overtime Hours")
+plt.title("Average Yearly Overtime Hours Per Employee (2012-2022)")
+plt.grid(True)
+plt.savefig('./figures/avg-overtime-per-employee.png')
+plt.close()
+
+# Total overtime plot
+plt.figure(figsize=(10, 5))
+plt.plot(years, totals, marker='o', linestyle='-', color='b')
+plt.xlabel("Year")
+plt.ylabel("Total Overtime Hours")
+plt.title("Total Yearly Overtime Hours (2012-2022)")
+plt.grid(True)
+plt.savefig('./figures/total-overtime.png')
+plt.close()
